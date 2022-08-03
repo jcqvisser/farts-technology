d5e5181b0a3d1724a6113ee9b423f440531c56c8
jcqvisser <jcqvisser@gmail.com>
2022-08-03 10:51:58 +0200

    create a front-end

---
+++ apps/farts-technology-front-end/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'farts-technology-front-end',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/farts-technology-front-end',
+};
