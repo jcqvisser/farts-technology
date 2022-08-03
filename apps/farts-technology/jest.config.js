7b5266b2616246d5af84d17f0b051fb9f6534d9f
jcqvisser <jcqvisser@gmail.com>
2022-08-03 10:46:30 +0200

    initial commit

---
+++ apps/farts-technology/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'farts-technology',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/farts-technology',
+};
